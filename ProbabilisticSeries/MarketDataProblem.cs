6451d3e2f16c749143387c484d91bdcbb19e3a7e
John R. Kosinski <john.kosinski@gmail.com>
2016-04-18 04:16:10 +0700

    no message

--- ProbabilisticSeries/MarketDataProblem.cs
+++ ProbabilisticSeries/MarketDataProblem.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +18,12 @@ using Trading.Utilities.TradingSystems;
 //#: represent the breakout selector as something dynamic and reusable 
 //#: test the results by running a trading system from them dynamically
 //#: encode as trinary (-1, 0, 1) to save processing 
+//#: break code into separate files 
+//#: generify writeOutput;
+//#: try multithreading 
 
-//TODO: generify writeOutput;
-//TODO: break code into separate files 
 //TODO: test carefully on very simple datasets that you can verify their correctness manually
 //TODO: evaluate trading systems based on different factors including drawdown and win percentage
-//TODO: try multithreading 
 //TODO: use a stop order definition 
 //TODO: optimize inside probabilityrunner
 //TODO: organize the relationship between features & keys better 
@@ -36,143 +37,158 @@ namespace ProbabilisticSeries
 {
     class MarketDataProblem
     {
-        public const string OUTPUT_FILE = "output.txt"; 
+        private const string OUTPUT_FILE = "output.txt";
+        private const string DATA_DIRECTORY = @"D:\MarketData\";
+        private const int PARALLEL_MAX = 4;
+
+        private static object _syncLock = new object(); 
 
         public static void Run()
         {
             //delete old log file 
-            System.IO.File.Delete(OUTPUT_FILE);
+            //System.IO.File.Delete(OUTPUT_FILE);
 
-            Dictionary<string, List<ProbabilityVector>> results = new Dictionary<string, List<ProbabilityVector>>();
-            Random rand = new Random() ;
-            ProbabilityVector bestSoFar = new ProbabilityVector();
+            int epoch = 0; 
+            var results = new Dictionary<string, List<ProbabilityVector>>(); 
+            var rand = new Random() ;
+            var bestSoFar = new ProbabilityVector();
             double bestRatingSoFar = 0;
 
             while (true)
             {
-                try
+                Parallel.For(0, PARALLEL_MAX, (nn) => 
                 {
-                    ProbabilityVector bestThisRound = new ProbabilityVector();
-                    double bestRatingThisRound = 0;
+                    var outputQueue = new OutputQueue(OUTPUT_FILE); 
 
-                    var runner = new MarketDataRunner();
-
-                    //set up the parameters 
-                    int breakoutLength = 3; // rand.Next(2, 9);
-                    int shortMaLen = rand.Next(7, 21);
-                    int longMaLen = rand.Next(shortMaLen, 80);
-                    int trendLen = rand.Next(2, 10);
-                    runner.SeriesMaxLen = 3;
-
-                    string key = String.Format("{0}|{1}|{2}|{3}", breakoutLength, shortMaLen, longMaLen, trendLen);
-
-                    //read in the training data 
-                    List<DataSet> dataSets = new List<DataSet>();
-                    CsvDataReader reader = new CsvDataReader() { OpenIndex = 1, HighIndex = 2, LowIndex = 3, CloseIndex = 4, DateIndex = 0, VolumeIndex = 5, RowStartIndex = 1 };
-                    dataSets.Add(reader.Read(@"e:\MarketData\t.csv"));
-                    dataSets.Add(reader.Read(@"e:\MarketData\c.csv"));
-                    dataSets.Add(reader.Read(@"e:\MarketData\bac.csv"));
-                    dataSets.Add(reader.Read(@"e:\MarketData\f.csv"));
-                    dataSets.Add(reader.Read(@"e:\MarketData\goog.csv"));
-                    dataSets.Add(reader.Read(@"e:\MarketData\ge.csv"));
-                    dataSets.Add(reader.Read(@"e:\MarketData\gs.csv"));
-                    dataSets.Add(reader.Read(@"e:\MarketData\ibm.csv"));
-                    dataSets.Add(reader.Read(@"e:\MarketData\jpm.csv"));
-                    dataSets.Add(reader.Read(@"e:\MarketData\cat.csv"));
-
-
-                    //set up the indicators 
-                    DataFeatureDefinition definition = new DataFeatureDefinition();
-                    definition.GoalIndicator =
-                        new IndicatorOutput()
-                        {
-                            Indicator = new BreakoutIndicator2(breakoutLength),
-                            OutputProperty = (i) => { return ((BreakoutIndicator2)i).Output; }
-                        };
-
-                    definition.Indicators.Add("isUp",
-                        new IndicatorOutput()
-                        {
-                            Indicator = new UpDownIndicator(),
-                            OutputProperty = (i) => { return ((UpDownIndicator)i).Output; }
-                        });
-
-                    TrendIndicator trendIndicator = new TrendIndicator(shortMaLen, longMaLen, trendLen);
-                    definition.Indicators.Add("trendUp",
-                        new IndicatorOutput()
-                        {
-                            Indicator = trendIndicator,
-                            OutputProperty = (i) => { return ((TrendIndicator)i).TrendUp; }
-                        });
-                    definition.Indicators.Add("shortMaOverLong",
-                        new IndicatorOutput()
-                        {
-                            Indicator = trendIndicator,
-                            OutputProperty = (i) => { return ((TrendIndicator)i).ShortMaOverLong; }
-                        });
-                    definition.Indicators.Add("newHigh",
-                        new IndicatorOutput()
-                        {
-                            Indicator = trendIndicator,
-                            OutputProperty = (i) => { return ((TrendIndicator)i).NewHigh; }
-                        });
-
-
-                    if (!results.ContainsKey(key))
+                    try
                     {
-                        runner.Run(dataSets, definition);
-                        var best = runner.SelectVectors(200, 0.12).OrderBy(p => p.Probability).Reverse().ToList();
+                        ProbabilityVector bestThisRound = new ProbabilityVector();
+                        double bestRatingThisRound = 0;
 
-                        results.Add(key, best);
+                        var runner = new MarketDataRunner();
 
-                        if (best.Count > 0)
-                        {
-                            Console.WriteLine("BreakoutLength: {0}", breakoutLength);
-                            Console.WriteLine("ShortMaLen: {0}", shortMaLen);
-                            Console.WriteLine("LongMaLen: {0}", longMaLen);
-                            Console.WriteLine("TrendLen: {0}", trendLen);
-                            Console.WriteLine("SeriesMaxLen: {0}", runner.SeriesMaxLen);
-                            
-                            foreach (var b in best)
+                        //set up the parameters 
+                        int breakoutLength = 3; // rand.Next(2, 9);
+                        int shortMaLen = rand.Next(7, 21);
+                        int longMaLen = rand.Next(shortMaLen, 80);
+                        int trendLen = rand.Next(2, 10);
+                        runner.SeriesMaxLen = 3;
+
+                        string key = String.Format("{0}|{1}|{2}|{3}", breakoutLength, shortMaLen, longMaLen, trendLen);
+
+                        //read in the training data 
+                        List<DataSet> dataSets = new List<DataSet>();
+                        CsvDataReader reader = new CsvDataReader() { OpenIndex = 1, HighIndex = 2, LowIndex = 3, CloseIndex = 4, DateIndex = 0, VolumeIndex = 5, RowStartIndex = 1 };
+                        dataSets.Add(reader.Read(DATA_DIRECTORY + "t.csv"));
+                        dataSets.Add(reader.Read(DATA_DIRECTORY + "c.csv"));
+                        dataSets.Add(reader.Read(DATA_DIRECTORY + "bac.csv"));
+                        dataSets.Add(reader.Read(DATA_DIRECTORY + "f.csv"));
+                        dataSets.Add(reader.Read(DATA_DIRECTORY + "goog.csv"));
+                        dataSets.Add(reader.Read(DATA_DIRECTORY + "ge.csv"));
+                        dataSets.Add(reader.Read(DATA_DIRECTORY + "gs.csv"));
+                        dataSets.Add(reader.Read(DATA_DIRECTORY + "ibm.csv"));
+                        dataSets.Add(reader.Read(DATA_DIRECTORY + "jpm.csv"));
+                        dataSets.Add(reader.Read(DATA_DIRECTORY + "cat.csv"));
+
+
+                        //set up the indicators 
+                        DataFeatureDefinition definition = new DataFeatureDefinition();
+                        definition.GoalIndicator =
+                            new IndicatorOutput()
                             {
-                                string outputLine = key + " " + b;
-                                Console.WriteLine(outputLine);
-                                System.IO.File.AppendAllText(OUTPUT_FILE, outputLine + "\n");
+                                Indicator = new BreakoutIndicator2(breakoutLength),
+                                OutputProperty = (i) => { return ((BreakoutIndicator2)i).Output; }
+                            };
 
-                                //run a trading system 
-                                definition.Predictors = PredictorsFromKey(b.Key);
-                                var r = TestTrader(dataSets, definition, breakoutLength);
+                        definition.Indicators.Add("isUp",
+                            new IndicatorOutput()
+                            {
+                                Indicator = new UpDownIndicator(),
+                                OutputProperty = (i) => { return ((UpDownIndicator)i).Output; }
+                            });
 
-                                //if (b.Probability > bestSoFar.Probability)    {
-                                var rating = RateTradingResults(r);
-                                if ((rating) > bestRatingSoFar)
+                        TrendIndicator trendIndicator = new TrendIndicator(shortMaLen, longMaLen, trendLen);
+                        definition.Indicators.Add("trendUp",
+                            new IndicatorOutput()
+                            {
+                                Indicator = trendIndicator,
+                                OutputProperty = (i) => { return ((TrendIndicator)i).TrendUp; }
+                            });
+                        definition.Indicators.Add("shortMaOverLong",
+                            new IndicatorOutput()
+                            {
+                                Indicator = trendIndicator,
+                                OutputProperty = (i) => { return ((TrendIndicator)i).ShortMaOverLong; }
+                            });
+                        definition.Indicators.Add("newHigh",
+                            new IndicatorOutput()
+                            {
+                                Indicator = trendIndicator,
+                                OutputProperty = (i) => { return ((TrendIndicator)i).NewHigh; }
+                            });
+
+
+                        if (!results.ContainsKey(key))
+                        {
+                            runner.Run(dataSets, definition);
+                            var best = runner.SelectVectors(200, 0.12).OrderBy(p => p.Probability).Reverse().ToList();
+
+                            results.Add(key, best);
+
+                            if (best.Count > 0)
+                            {
+                                outputQueue.AppendLine("BreakoutLength: {0}", breakoutLength);
+                                outputQueue.AppendLine("ShortMaLen: {0}", shortMaLen);
+                                outputQueue.AppendLine("LongMaLen: {0}", longMaLen);
+                                outputQueue.AppendLine("TrendLen: {0}", trendLen);
+                                outputQueue.AppendLine("SeriesMaxLen: {0}", runner.SeriesMaxLen);
+
+                                foreach (var b in best)
                                 {
-                                    bestRatingSoFar = rating;
-                                    bestSoFar = b;
+                                    string outputLine = key + " " + b;
+                                    outputQueue.AppendLine(outputLine);
+
+                                    //run a trading system 
+                                    definition.Predictors = PredictorsFromKey(b.Key);
+                                    var r = TestTrader(dataSets, definition, breakoutLength);
+                                    var rating = RateTradingResults(r);
+
+                                    lock (_syncLock)
+                                    {
+                                        //if (b.Probability > bestSoFar.Probability)    {
+                                        if ((rating) > bestRatingSoFar)
+                                        {
+                                            bestRatingSoFar = rating;
+                                            bestSoFar = b;
+                                        }
+
+                                        if ((rating) > bestRatingThisRound)
+                                        {
+                                            bestRatingThisRound = rating;
+                                            bestThisRound = b;
+                                        }
+                                    }
+
+                                    outputQueue.AppendLine(" * {0} - {1} : {2}", r.PercentGain, r.AverageDrawdown, rating);
                                 }
 
-                                if ((rating) > bestRatingThisRound)
-                                {
-                                    bestRatingThisRound = rating;
-                                    bestThisRound = b;
-                                }
-
-                                Console.WriteLine(" * {0} - {1} : {2}", r.PercentGain, r.AverageDrawdown, rating);
+                                outputQueue.AppendLine();
+                                outputQueue.AppendLine("Best so far: " + bestSoFar.ToString() + " " + bestRatingSoFar.ToString());
+                                outputQueue.AppendLine("Best this round: " + bestThisRound.ToString() + " " + bestRatingThisRound.ToString());
                             }
 
-                            Console.WriteLine();
-                            Console.WriteLine("Best so far: " + bestSoFar.ToString() + " " + bestRatingSoFar.ToString());
-                            Console.WriteLine("Best this round: " + bestThisRound.ToString() + " " + bestRatingThisRound.ToString());
-                            System.IO.File.AppendAllText(OUTPUT_FILE, bestSoFar.ToString() + "\n");
+                            outputQueue.AppendLine();
                         }
-
-                        Console.WriteLine();
                     }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.ToString());
-                }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+
+                    epoch++;
+                    outputQueue.AppendLine("END EPOCH " + epoch.ToString());
+                    outputQueue.Flush(); 
+                }); 
             }
         }
 
@@ -336,6 +352,45 @@ namespace ProbabilisticSeries
         static double RateTradingResults(TradingResults results)
         {
             return (results.PercentGain - results.AverageDrawdown);
+        }
+
+
+        class OutputQueue
+        {
+            private static object _syncLock = new object();
+            private static string _crlf = ASCIIEncoding.ASCII.GetString(new byte[] { 13, 10 }); 
+            private List<string> _lines = new List<string>();
+            private string _outputFile;
+
+            public OutputQueue(string outputFile)
+            {
+                _outputFile = outputFile;
+            }
+
+            public void AppendLine()
+            {
+                this.AppendLine(String.Empty); 
+            }
+
+            public void AppendLine(string format, params object[] args)
+            {
+                _lines.Add(String.Format(format, args)); 
+            }
+
+            public void Flush()
+            {
+                lock (_syncLock)
+                {
+                    foreach (string line in _lines)
+                    {
+                        Console.WriteLine(line);
+                        System.Diagnostics.Debug.WriteLine(line);
+                    }
+
+                    //if (_outputFile != null)
+                    //    System.IO.File.AppendAllLines(_outputFile, _lines);
+                }
+            }
         }
     }
 }
